dc5d811ca4966d634a65a5e1ed4a40479f5856d1
Scott <scottlewwork@gmail.com>
2024-10-07 17:55:56 +0100

    Started projects section

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,9 +7,30 @@ function SplitScroll() {
         triggerHook: 0
     })
     .setPin('.aboutMeTitle')
-    .addTo(scrollController)
+    .addTo(scrollController);
 }
 
 $(document).ready(function() {
     SplitScroll();
+
+    const track = document.getElementById("projectImageTrack")
+    window.onmousedown = e => {
+        track.dataset.mouseDownAt = e.clientX;
+    }
+    window.onmousemove = e => {
+        if(track.dataset.mouseDownAt === "0") return;
+    
+        const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
+        
+        const percentage = (mouseDelta / maxDelta) * -100;
+        const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+        track.dataset.percentage = nextPercentage;
+        track.style.transform = `translate(${nextPercentage}%, 0%)`;
+    }
+    window.onmouseup = () => {
+        track.dataset.mouseDownAt = "0";
+        track.dataset.prevPercentage = track.dataset.percentage;
+    }
+
 });
