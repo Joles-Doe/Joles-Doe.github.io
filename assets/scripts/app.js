3b94412e11e39af92722640facf09f180beb2345
Scott <scottlewwork@gmail.com>
2024-10-23 15:17:36 +0100

    Fixed AboutMe whitespace, added favicons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,35 @@
+//Function to add the ScrollMagic sticky section
 function SplitScroll() {
     const scrollController = new ScrollMagic.Controller();
 
-    var container = document.querySelector('#aboutMe');
-    var containerHeight = container.offsetHeight;
+    var pagesHeight = document.getElementById('aboutMePages').offsetHeight;
+    var containerHeight = document.getElementById('aboutMeTitle').offsetHeight;
 
     var scrollScene = new ScrollMagic.Scene({
-        duration: containerHeight,
-        triggerElement: '.aboutMeTitle',
+        duration: pagesHeight - containerHeight,
+        triggerElement: '#aboutMeTitle',
         triggerHook: 0
     })
-    .setPin('.aboutMeTitle')
+    .setPin('#aboutMeTitle')
     .addTo(scrollController)
+    .addIndicators()
 
     return scrollScene;
 }
 
+//The aboutme div likes to create whitespace, so this function gives it an absolute height
+function ResizeAboutMe() {
+    var pageOneHeight = document.getElementById('aboutMePageOne').offsetHeight;
+    var pageTwoHeight = document.getElementById('aboutMePageTwo').offsetHeight;
+
+    var aboutMe = document.getElementById('aboutMe');
+    var aboutMePages = document.getElementById('aboutMePages');
+    aboutMe.style.height = pageOneHeight + pageTwoHeight + 'px';
+    aboutMePages.style.height = pageOneHeight + pageTwoHeight + 'px'; 
+}
+
 $(document).ready(function() {
+    ResizeAboutMe();
     var scrollScene = SplitScroll();
     var navbarHeight = document.getElementById('navbar').offsetHeight;
 
@@ -25,9 +39,11 @@ $(document).ready(function() {
         {
             scrollScene.destroy(true);
         }
+        ResizeAboutMe();
         scrollScene = SplitScroll();
     })
 
+    //Event listeners for the navbar buttons
     document.getElementById('titleButton').addEventListener('click', function() {
         const target = document.getElementById('title');
         const targetPosition = target.getBoundingClientRect().top + window.scrollY - navbarHeight;
@@ -52,6 +68,8 @@ $(document).ready(function() {
             behaviour: 'smooth'
         })
     });
+
+    //Event listeners for the contact me buttons
     document.getElementById('contactmeButton').addEventListener('click', function() {
         const target = document.getElementById('contactMe');
         const targetPosition = target.getBoundingClientRect().top + window.scrollY - (navbarHeight + 5);
@@ -60,36 +78,8 @@ $(document).ready(function() {
             behaviour: 'smooth'
         })
     });
-
     document.getElementById("emailContainer").addEventListener("click", function() {
         const email = "contactme.scottlewis@gmail.com";
         window.location.href = `mailto:${email}`;
     });
 });
-
-// function UpdateDuration(scrollScene) {
-//     var container = document.querySelector('.aboutMe');
-//     var containerHeight = container.offsetHeight;
-//     scrollScene.duration(containerHeight);
-// }
-
-
-// const track = document.getElementById("projectImageTrack")
-// window.onmousedown = e => {
-//     track.dataset.mouseDownAt = e.clientX;
-// }
-// window.onmousemove = e => {
-//     if(track.dataset.mouseDownAt === "0") return;
-
-//     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
-    
-//     const percentage = (mouseDelta / maxDelta) * -100;
-//     const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-
-//     track.dataset.percentage = nextPercentage;
-//     track.style.transform = `translate(${nextPercentage}%, 0%)`;
-// }
-// window.onmouseup = () => {
-//     track.dataset.mouseDownAt = "0";
-//     track.dataset.prevPercentage = track.dataset.percentage;
-// }
